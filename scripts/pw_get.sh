62190f085d25b18da3f93156f7d72f43b2269470
Aidan Loughran <aidlo.dev@pm.me>
2022-11-07 00:25:49 +0000

    `pw_get`

---
+++ scripts/pw_get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -eq 0 ]; then
+  echo "No argument given."
+  exit 1
+elif [ "$#" -gt 1 ]; then
+  echo "Additional arguments ignored."
+fi
+
+gpg -d ~/documents/pw.gpg | grep -i "$1"

96fc1a3273bfa642f34f84d84892a61178b858a2
Aidan Loughran <aidlo.dev@pm.me>
2022-11-17 21:09:32 +0000

    Phone backup script

---
+++ home/admin/scripts/backup_phone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script is ran manually after cd-ing into mounted
+# dir where .SeedVaultAndroidBackup/ can be found
+
+# Create tar and pipe it through GPG
+tar -czvf - .SeedVaultAndroidBackup/ | gpg \
+  -se -r "aidlo.dev@pm.me" \
+  -z 0 --cipher-algo aes256 \
+  -o /mnt/nas/backup/android/$(date '+%F').tgz.gpg
